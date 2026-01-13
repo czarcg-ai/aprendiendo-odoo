0e99d80f0e0c09b7c88196476e19a5b67c3fafb8
czarcg-ai <czarcg@gmail.com>
2026-01-13 22:04:46 +0000

    Llegaron los gringos

--- prueba.py
+++ prueba.py
@@ -14,12 +14,14 @@ class personaje:
 
 # --- ZONA DE EJECUCIÓN ---
 # Creamos al primer personaje usando el molde 'personaje'
+
 hp1 = personaje("Maduro cds", 100)
 # Creamos al SEGUNDO personaje usando EL MISMO molde 'personaje'
 # (Solo cambiamos el nombre de la variable a la izquierda)
 hp2 = personaje("Diosdi hp", 100)
-hp1.Coñazo()
-print(f"¡misilazos pa miraflores {hp1.nombre}!")
+print(f"¡misilazos pa miraflores {hp1.nombre}!") #pero no puede ir antes de la creación de los personajes xd
+hp1.Coñazo() #el print tiene que ir antes que esto para que se vea bien xd
+
 while hp1.vida > 0:
     hp1.Coñazo()  
     time.sleep(1)
